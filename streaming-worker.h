a26c27976dd1cb0fe866e9e094154c45b067e44a
Bernd Kaiser <bk@dfjk.eu>
2020-09-18 09:46:01 +0200

    streaming-worker: fix nan deprecation warning
    Calling NaN::Callback without an AsyncResource has been depracted.
    
    Added async resource for every Callback->call()

--- streaming-worker.h
+++ streaming-worker.h
@@ -33,109 +33,120 @@ SOFTWARE.
 #include <condition_variable>
 #include <nan.h>
 
-
 using namespace Nan;
 using namespace std;
 
-template<typename Data>
+template <typename Data>
 class PCQueue
 {
 public:
-    void write(Data data) {
-        while (true) {
-            std::unique_lock<std::mutex> locker(mu);
-            buffer_.push_back(data);
-            locker.unlock();
-            cond.notify_all();
-            return;
-        }
+  void write(Data data)
+  {
+    while (true)
+    {
+      std::unique_lock<std::mutex> locker(mu);
+      buffer_.push_back(data);
+      locker.unlock();
+      cond.notify_all();
+      return;
     }
-    Data read() {
-        while (true)
-        {
-            std::unique_lock<std::mutex> locker(mu);
-            cond.wait(locker, [this](){return buffer_.size() > 0;});
-            Data back = buffer_.front();
-            buffer_.pop_front();
-            locker.unlock();
-            cond.notify_all();
-            return back;
-        }
+  }
+  Data read()
+  {
+    while (true)
+    {
+      std::unique_lock<std::mutex> locker(mu);
+      cond.wait(locker, [this]() { return buffer_.size() > 0; });
+      Data back = buffer_.front();
+      buffer_.pop_front();
+      locker.unlock();
+      cond.notify_all();
+      return back;
     }
-    void readAll(std::deque<Data> & target) {
-        std::unique_lock<std::mutex> locker(mu);
-        std::copy(buffer_.begin(), buffer_.end(),  std::back_inserter(target));
-        buffer_.clear();
-        locker.unlock();
-    }
-    PCQueue() {}
+  }
+  void readAll(std::deque<Data> &target)
+  {
+    std::unique_lock<std::mutex> locker(mu);
+    std::copy(buffer_.begin(), buffer_.end(), std::back_inserter(target));
+    buffer_.clear();
+    locker.unlock();
+  }
+  PCQueue() {}
+
 private:
-    std::mutex mu;
-    std::condition_variable cond;
-    std::deque<Data> buffer_;
+  std::mutex mu;
+  std::condition_variable cond;
+  std::deque<Data> buffer_;
 };
 
-
-class Message {
+class Message
+{
 public:
   string name;
   string data;
   string binary;
-  Message(string name, string data, string binary) : name(name), data(data), binary(binary){}
+  Message(string name, string data, string binary) : name(name), data(data), binary(binary) {}
 };
 
-class StreamingWorker : public AsyncProgressWorker {
- public:
+class StreamingWorker : public AsyncProgressWorker
+{
+public:
   StreamingWorker(
       Callback *progress,
-      Callback *callback, 
-      Callback *error_callback
-    )
-    : AsyncProgressWorker(callback), progress(progress), error_callback(error_callback)
-    {
-      input_closed = false;
-    }
+      Callback *callback,
+      Callback *error_callback)
+      : AsyncProgressWorker(callback), progress(progress), error_callback(error_callback)
+  {
+    input_closed = false;
+  }
 
-  ~StreamingWorker() {
+  ~StreamingWorker()
+  {
     delete progress;
     delete error_callback;
   }
 
-  void HandleErrorCallback() {
+  void HandleErrorCallback()
+  {
     HandleScope scope;
 
     v8::Local<v8::Value> argv[] = {
-      v8::Exception::Error(New<v8::String>(ErrorMessage()).ToLocalChecked())
-    };
-    error_callback->Call(1, argv);
+        v8::Exception::Error(New<v8::String>(ErrorMessage()).ToLocalChecked())};
+
+    Nan::AsyncResource async_resource("streaming-worker:error");
+    error_callback->Call(1, argv, &async_resource);
   }
 
-  void HandleOKCallback() {
+  void HandleOKCallback()
+  {
     drainQueue();
-    callback->Call(0, NULL);
+    Nan::AsyncResource async_resource("streaming-worker:okay");
+    callback->Call(0, NULL, &async_resource);
   }
 
-  void HandleProgressCallback(const char *data, size_t size) {
+  void HandleProgressCallback(const char *data, size_t size)
+  {
     drainQueue();
   }
 
-  void close() {
+  void close()
+  {
     input_closed = true;
   }
 
   PCQueue<Message> fromNode;
 
- protected:
-
-  void writeToNode(const AsyncProgressWorker::ExecutionProgress& progress, const Message & msg){
+protected:
+  void writeToNode(const AsyncProgressWorker::ExecutionProgress &progress, const Message &msg)
+  {
     toNode.write(msg);
-    progress.Send(reinterpret_cast<const char*>(&toNode), sizeof(toNode));
+    progress.Send(reinterpret_cast<const char *>(&toNode), sizeof(toNode));
   }
 
-  bool closed() {
+  bool closed()
+  {
     return input_closed;
   }
-
 
   Callback *progress;
   Callback *error_callback;
@@ -143,103 +154,117 @@ class StreamingWorker : public AsyncProgressWorker {
   bool input_closed;
 
 private:
-  void drainQueue() {
+  void drainQueue()
+  {
     HandleScope scope;
 
     // drain the queue - since we might only get called once for many writes
     std::deque<Message> contents;
     toNode.readAll(contents);
 
-    for(Message & msg : contents) {
+    for (Message &msg : contents)
+    {
       auto eventName = New<v8::String>(msg.name.c_str()).ToLocalChecked();
       auto eventMessage = New<v8::String>(msg.data.c_str()).ToLocalChecked();
-      
-      if (msg.binary.length() > 0) {
+
+      if (msg.binary.length() > 0)
+      {
         v8::Local<v8::Value> argv[] = {
-          eventName, 
-          eventMessage,
-          CopyBuffer(&msg.binary[0], msg.binary.length()).ToLocalChecked()
-        };
-        progress->Call(3, argv);
-      } else {
+            eventName,
+            eventMessage,
+            CopyBuffer(&msg.binary[0], msg.binary.length()).ToLocalChecked()};
+        Nan::AsyncResource async_resource("streaming-worker:binary-message");
+        progress->Call(3, argv, &async_resource);
+      }
+      else
+      {
         v8::Local<v8::Value> argv[] = {
-          eventName, 
-          eventMessage
-        };
-        progress->Call(2, argv);
+            eventName,
+            eventMessage};
+        Nan::AsyncResource async_resource("streaming-worker:message");
+        progress->Call(2, argv, &async_resource);
       }
     }
   }
 };
 
-StreamingWorker * create_worker(Callback *, Callback *, Callback *, v8::Local<v8::Object> &);
+StreamingWorker *create_worker(Callback *, Callback *, Callback *, v8::Local<v8::Object> &);
 
-class StreamWorkerWrapper : public Nan::ObjectWrap {
- public:
-  static NAN_MODULE_INIT(Init) {
+class StreamWorkerWrapper : public Nan::ObjectWrap
+{
+public:
+  static NAN_MODULE_INIT(Init)
+  {
     v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
     tpl->SetClassName(Nan::New("StreamingWorker").ToLocalChecked());
     tpl->InstanceTemplate()->SetInternalFieldCount(2);
 
     // SetPrototypeMethod(tpl, "sendToAddon", sendToAddon);
     SetPrototypeMethod(tpl, "closeInput", closeInput);
-    
+
     constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
     Nan::Set(target, Nan::New("StreamingWorker").ToLocalChecked(),
-      Nan::GetFunction(tpl).ToLocalChecked());
+             Nan::GetFunction(tpl).ToLocalChecked());
   }
 
-  
-
- private:
-  explicit StreamWorkerWrapper(StreamingWorker * worker) : _worker(worker) {}
+private:
+  explicit StreamWorkerWrapper(StreamingWorker *worker) : _worker(worker) {}
   ~StreamWorkerWrapper() {}
 
-  static NAN_METHOD(New) {
-    if (info.IsConstructCall()) {
-      if (info[0]->IsUndefined() || !info[0]->IsFunction()) {
+  static NAN_METHOD(New)
+  {
+    if (info.IsConstructCall())
+    {
+      if (info[0]->IsUndefined() || !info[0]->IsFunction())
+      {
         ThrowError("No data callback");
         return;
       }
 
       Callback *data_callback = new Callback(info[0].As<v8::Function>());
 
-      if (info[1]->IsUndefined() || !info[1]->IsFunction()) {
+      if (info[1]->IsUndefined() || !info[1]->IsFunction())
+      {
         ThrowError("No complete callback");
         return;
       }
 
       Callback *complete_callback = new Callback(info[1].As<v8::Function>());
 
-      if (info[2]->IsUndefined() || !info[2]->IsFunction()) {
+      if (info[2]->IsUndefined() || !info[2]->IsFunction())
+      {
         ThrowError("No error callback");
         return;
       }
 
       Callback *error_callback = new Callback(info[2].As<v8::Function>());
-      
-      v8::Isolate* isolate = info.GetIsolate();
+
+      v8::Isolate *isolate = info.GetIsolate();
       v8::Local<v8::Object> options;
 
-      if (!info[3]->IsUndefined() && info[3]->IsObject()) {
+      if (!info[3]->IsUndefined() && info[3]->IsObject())
+      {
         options = info[3].As<v8::Object>();
-      } else {
+      }
+      else
+      {
         options = v8::Object::New(isolate);
       }
 
       StreamWorkerWrapper *obj = new StreamWorkerWrapper(
-                            create_worker(
-                                data_callback, 
-                                complete_callback, 
-                                error_callback, options));
-      
+          create_worker(
+              data_callback,
+              complete_callback,
+              error_callback, options));
+
       obj->Wrap(info.This());
       info.GetReturnValue().Set(info.This());
 
       // start the worker
       AsyncQueueWorker(obj->_worker);
-
-    } else {
+    }
+    else
+    {
       const int argc = 3;
       v8::Local<v8::Value> argv[argc] = {info[0], info[1], info[2]};
       v8::Local<v8::Function> cons = Nan::New(constructor());
@@ -255,16 +280,18 @@ class StreamWorkerWrapper : public Nan::ObjectWrap {
   //   obj->_worker->fromNode.write(Message(*name, *data));
   // }
 
-  static NAN_METHOD(closeInput) {
-    StreamWorkerWrapper* obj = Nan::ObjectWrap::Unwrap<StreamWorkerWrapper>(info.Holder());
+  static NAN_METHOD(closeInput)
+  {
+    StreamWorkerWrapper *obj = Nan::ObjectWrap::Unwrap<StreamWorkerWrapper>(info.Holder());
     obj->_worker->close();
   }
 
-  static inline Nan::Persistent<v8::Function> & constructor() {
+  static inline Nan::Persistent<v8::Function> &constructor()
+  {
     static Nan::Persistent<v8::Function> my_constructor;
     return my_constructor;
   }
 
-  StreamingWorker * _worker;
+  StreamingWorker *_worker;
 };
 #endif // ____StreamingWorker__
