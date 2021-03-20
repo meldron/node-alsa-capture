97b9869ca74554cdc0026918511a9eb5c850bea5
Bernd Kaiser <bk@dfjk.eu>
2021-03-20 02:36:12 +0100

    macros: DISABLE_WCAST_FUNCTION_TYPE comment.
    Fixes #5

--- macros.h
+++ macros.h
@@ -1,3 +1,9 @@
+/*
+ * this macro is used to silence nan warnings added with cast-function-type and introduced in GCC 8 (https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html)
+ * macro from Alba Mendez (@mildsunrise) https://github.com/nodejs/nan/issues/807#issuecomment-581536991
+ * see also https://github.com/meldron/node-alsa-capture/issues/5
+ */
+
 #if defined(__GNUC__) && __GNUC__ >= 8
 #define DISABLE_WCAST_FUNCTION_TYPE _Pragma("GCC diagnostic push") _Pragma("GCC diagnostic ignored \"-Wcast-function-type\"")
 #define DISABLE_WCAST_FUNCTION_TYPE_END _Pragma("GCC diagnostic pop")
